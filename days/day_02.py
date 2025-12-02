cca01b1fcdb19c25e735f2aa9445ef089c4911e6
Vera van den Heuvel <ray10k@xmsnet.nl>
2025-12-02 08:20:29 +0100

    day 2, star 1

--- days/day_02.py
+++ days/day_02.py
@@ -5,16 +5,45 @@ class InputItem:
     """Representation of one 'unit' of input-data. May represent as little
     as a single character from input, as much as the entire file, or anywhere
     inbetween."""
-    a: str
+    start:str
+    end:str
+    
+    def into_range(self) -> range:
+        return range(int(self.start),int(self.end)+1)
 
 IType = list[InputItem]
 """The "full" input-data. One input.txt file should parse into one IType"""
 
+def is_repeated(value:int) -> bool:
+    """Checks if the given number matches the conditions for star one.
+
+    Args:
+        value (int): A number that may be "invalid"
+
+    Returns:
+        bool: True if the number is "invalid", False otherwise.
+    """
+    str_value = str(value)
+    #Easy case: only even-length numbers can be invalid.
+    if len(str_value) % 2 != 0:
+        return False
+    halfway = len(str_value) // 2
+    return str_value[0:halfway] == str_value[halfway:]
+
 def parse_input(input_content:str) -> IType:
-    return list()
+    retval = list()
+    for input_range in input_content.split(","):
+        start, end = input_range.split("-")
+        retval.append(InputItem(start,end))
+    return retval
 
 def star_one(data:IType) -> str:
-    pass
+    retval = 0
+    for data_range in data:
+        for id_to_check in data_range.into_range():
+            if is_repeated(id_to_check):
+                retval += id_to_check
+    return str(retval)
 
 def star_two(data:IType) -> str:
     pass
