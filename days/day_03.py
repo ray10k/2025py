db9bf9e080f77bf7396ba59decaaa166a3e6fe91
Vera van den Heuvel <ray10k@xmsnet.nl>
2025-12-03 08:26:51 +0100

    day 3, star 1

--- days/day_03.py
+++ days/day_03.py
@@ -5,16 +5,30 @@ class InputItem:
     """Representation of one 'unit' of input-data. May represent as little
     as a single character from input, as much as the entire file, or anywhere
     inbetween."""
-    a: str
+    batteries:tuple[int,...]
 
 IType = list[InputItem]
 """The "full" input-data. One input.txt file should parse into one IType"""
 
 def parse_input(input_content:str) -> IType:
-    return list()
+    retval = list()
+    for bank in input_content.splitlines():
+        batteries = tuple(int(x) for x in bank.strip())
+        retval.append(InputItem(batteries))
+    return retval
 
 def star_one(data:IType) -> str:
-    pass
+    retval = 0
+    for battery_bank in data:
+        #step 1: find the highest number on the left of the bank.
+        left_highest = max(battery_bank.batteries[0:-1])
+        #step 2: find the index of that number.
+        splitpoint = battery_bank.batteries.index(left_highest) + 1
+        #step 3: find the highest number on the right of the bank.
+        right_highest = max(battery_bank.batteries[splitpoint:])
+        #step 4: combine and add.
+        retval += (left_highest*10) + right_highest
+    return str(retval)
 
 def star_two(data:IType) -> str:
     pass
