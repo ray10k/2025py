76df7a6df838b2787567fc18d67e362d8f6fbdcb
Vera van den Heuvel <ray10k@xmsnet.nl>
2025-12-05 10:56:50 +0100

    day 5, star 1

--- days/day_05.py
+++ days/day_05.py
@@ -1,20 +1,41 @@
 from dataclasses import dataclass;
 
 @dataclass
-class InputItem:
+class IdRange:
     """Representation of one 'unit' of input-data. May represent as little
     as a single character from input, as much as the entire file, or anywhere
     inbetween."""
-    a: str
+    start:int
+    end:int
+    
+    def into_range(self):
+        return range(self.start,self.end+1)
+    
+    def check_id(self,id:int) -> bool:
+        return id >= self.start and id <= self.end
 
-IType = list[InputItem]
+IType = tuple[list[IdRange],list[int]]
 """The "full" input-data. One input.txt file should parse into one IType"""
 
 def parse_input(input_content:str) -> IType:
-    return list()
+    lines = iter(input_content.splitlines())
+    ranges = list()
+    while (curr_line := next(lines)) != "":
+        left,right = curr_line.strip().split("-")
+        ranges.append(IdRange(int(left),int(right)))
+    ingredients = list(int(line) for line in lines)
+    return ranges, ingredients
 
 def star_one(data:IType) -> str:
-    pass
+    print(len(data[0]),len(data[1]))
+    ranges, ingredients = data
+    retval = 0
+    for ingredient in ingredients:
+        for id_range in ranges:
+            if id_range.check_id(ingredient):
+                retval += 1
+                break
+    return str(retval)
 
 def star_two(data:IType) -> str:
     pass
