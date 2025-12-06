c8973d472af9ec9e6f1a153e5c322d48224c991c
Vera van den Heuvel <ray10k@xmsnet.nl>
2025-12-06 13:49:00 +0100

    day 6, star 1

--- days/day_06.py
+++ days/day_06.py
@@ -1,23 +1,53 @@
-from dataclasses import dataclass;
+import re
 
-@dataclass
-class InputItem:
-    """Representation of one 'unit' of input-data. May represent as little
-    as a single character from input, as much as the entire file, or anywhere
-    inbetween."""
-    a: str
 
-IType = list[InputItem]
-"""The "full" input-data. One input.txt file should parse into one IType"""
+
+IType = tuple[list[list[int]],list[list[int]],list[bool]]
+"""The "full" input-data. One input.txt file should parse into one IType. The contents
+are the grid of horizontal numbers, the grid of vertical numbers and a list of operations; 
+True means add, False means multiply."""
 
 def parse_input(input_content:str) -> IType:
-    return list()
+    grid = list()
+    operations = list()
+    number_pt = re.compile(r"\d+")
+    
+    lines = input_content.strip().splitlines()
+    column_len = max(len(line) for line in lines)
+    
+    for num_line in lines[:-1]:
+        numbers = list(int(num) for num in number_pt.findall(num_line))
+        grid.append(numbers)
+    
+    #Well, that's a first for me. Turns out that star 2 is a *parsing* challenge.
+    grid_2 = list()
+    
+    for x in range(column_len):
+        v_num = "".join(lines[y][x] for y in range(len(lines)-1))
+        
+    
+    for operation in lines[-1].strip():
+        if operation not in "*+":
+            continue
+        operations.append(operation == "+")
+    
+    return grid, grid_2, operations
 
 def star_one(data:IType) -> str:
-    pass
+    grid, _, operations = data
+    results:list[int] = list(int(not op) for op in operations) #Multiply columns need to start at 1, add columns at 0.
+
+    for row in grid:
+        for x, column in enumerate(row):
+            if operations[x]:
+                results[x] += column
+            else:
+                results[x] *= column
+    return str(sum(results))
 
 def star_two(data:IType) -> str:
-    pass
+    _, grid, operations = data
+    return star_one((grid, None, operations))
 
 if __name__ == "__main__":
     from pathlib import Path
