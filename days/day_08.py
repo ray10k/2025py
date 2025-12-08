66584299bebd3d7b46e9484618ff8e4af04e2dbc
Vera van den Heuvel <ray10k@xmsnet.nl>
2025-12-08 09:35:04 +0100

    day 8, star 1

--- days/day_08.py
+++ days/day_08.py
@@ -1,20 +1,70 @@
-from dataclasses import dataclass;
+from dataclasses import dataclass
+from itertools import combinations
 
-@dataclass
-class InputItem:
-    """Representation of one 'unit' of input-data. May represent as little
-    as a single character from input, as much as the entire file, or anywhere
-    inbetween."""
-    a: str
+@dataclass(frozen=True,slots=True,order=True)
+class Position:
+    """3D coordinate."""
+    x:int
+    y:int
+    z:int
+    
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y}, {self.z})"
+    
+    def distance(self,other:"Position") -> float:
+        delta_x = (self.x - other.x) ** 2
+        delta_y = (self.y - other.y) ** 2
+        delta_z = (self.z - other.z) ** 2
+        return (delta_x + delta_y + delta_z) ** 0.5
 
-IType = list[InputItem]
+IType = list[Position]
 """The "full" input-data. One input.txt file should parse into one IType"""
 
 def parse_input(input_content:str) -> IType:
-    return list()
+    retval = list()
+    for line in input_content.splitlines():
+        coords = list(int(c) for c in line.strip().split(","))
+        retval.append(Position(*coords))
+    return retval
 
 def star_one(data:IType) -> str:
-    pass
+    distance_cache:dict[tuple[Position,Position],float] = dict()
+    for first,second in combinations(data,2): # 499500 operations; should be OK?
+        key = min(first,second),max(first,second)
+        value = first.distance(second)
+        distance_cache[key] = value
+    nearest_pairs = sorted(distance_cache.keys(),key=lambda pair: distance_cache[pair])
+    #Can't really test this easily; the example expects 10 connections, while the real run expects 1000
+    #So, improvise.
+    is_test = len(data) == 20
+    
+    circuits:list[set[Position]] = list()
+    max_connections = 10 if is_test else 1000
+    for left, right in nearest_pairs[0:max_connections]:
+        l_circ = None
+        r_circ = None
+        for circuit in circuits:
+            if left in circuit:
+                l_circ = circuit
+            if right in circuit:
+                r_circ = circuit
+        if l_circ is not None and r_circ is not None and l_circ != r_circ:
+            #Merge the circuits. Both nodes are already included anyway.
+            circuits.remove(l_circ)
+            circuits.remove(r_circ)
+            new_circuit = l_circ.union(r_circ)
+            circuits.append(new_circuit)
+        elif l_circ is not None:
+            #Add the new node to the existing circuit.
+            l_circ.add(right)
+        elif r_circ is not None:
+            r_circ.add(left)
+        else:
+            #Create a new circuit.
+            circuits.append(set([left,right]))
+    sizes = sorted((len(circuit) for circuit in circuits),reverse=True)
+    retval = sizes[0] * sizes[1] * sizes[2]
+    return str(retval)
 
 def star_two(data:IType) -> str:
     pass
